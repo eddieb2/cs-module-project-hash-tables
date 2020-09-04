bac863b60590295296cf82935c0e1d3664961577
Edward Blanciak <blanciak.edward2@gmail.com>
2020-09-03 20:23:24 -0400

    partially complete

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,24 @@
 def word_count(s):
-    # Your code here
+    cache = {}
 
+    spl = s.split(" ")
+
+    # print(spl)
+    words = []
+    for i in spl:
+        s = ''.join(char for char in i if char.isalnum())
+        words.append(s.lower())
+
+
+    for word in words:
+        if word.isalpha() is False:
+            continue
+        if word not in cache:
+            cache[word] = 0
+
+        cache[word] += 1
+
+    return cache
 
 
 if __name__ == "__main__":
@@ -8,3 +26,9 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
+    print(word_count('a a\ra\na\ta \t\r\n'))
+
+    # print(cache)
+
+    # (x == {'hello': 2, 'my': 2, 'cat': 2, 'and': 1, "doesn't": 1, 'say': 1, 'back': 1})
